936f5a9a21871b4f95ca84168def9a94938eac7d
Victor Santos <victorsantos@Itzels-iMac.lan>
2023-12-13 15:13:12 -0500

    Cart modified

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,12 +1,19 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
 const CartItem = ({item}) => {
-  const{is, title, image, price, amount} = item;
+  const{id, title, image, price, amount} = item;
   return(
   <div className= 'flex' >
 <div className='w-full min-h-[150px] flex items-center gap-x-4'>
   <div>
-    <img className= 'max-w-[80px]' src={image} alt="" />
+    <Link to={`/product/${id}`}>
+    <img className= 'max-w-[80px]' src={image} alt='' />
+    </Link>
+    <div>
+      <div>
+        <Link to={`/product/${id}`}> {title} </Link>
+      </div>
+    </div>
   </div>
 </div>
 
